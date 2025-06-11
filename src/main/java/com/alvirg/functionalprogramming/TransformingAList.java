2397bd9d0b2aeea42a9408dda4cff99c39608e69
albertegi <albertegi90@gmail.com>
2025-06-11 23:59:04 +0100

    added more files and a map method to transform a List

---
+++ src/main/java/com/alvirg/functionalprogramming/TransformingAList.java
@@ -0,0 +1,25 @@
+package com.alvirg.functionalprogramming;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TransformingAList {
+    public static void main(String[] args) {
+
+
+        final List<String> friends = Arrays.asList(
+                "Brian",
+                "Nate",
+                "Neal",
+                "Raju",
+                "Sara",
+                "Scott");
+
+
+        friends.stream()
+                .map(name->name.toUpperCase())
+                .forEach(name->System.out.print(name + " "));
+        System.out.println();
+
+    }
+}
